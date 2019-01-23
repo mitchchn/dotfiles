cf41dc9989c98f7b96999e440078059b1dd7d0a4
Mitchell Cohen <mitch@agilebits.com>
2019-01-23 16:40:02 -0500

    shortened spacefish prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,9 @@
-set PRIVATE_CONFIG_FILE $HOME/.config/fish/config.private.fish
+set PRIVATE_CONFIG_FILE = $HOME/.config/fish/config.private.fish
 if test -e $PRIVATE_CONFIG_FILE
     source $PRIVATE_CONFIG_FILE
 end
+
+### Spacefish
+# Options: https://github.com/matchai/spacefish/blob/master/docs/Options.md
+# set SPACEFISH_PROMPT_ORDER time user dir host git package node docker ruby golang php rust haskell julia aws conda pyenv kubecontext exec_time line_sep battery jobs exit_code char
+set SPACEFISH_PROMPT_ORDER time user dir host git exec_time line_sep jobs exit_code char
