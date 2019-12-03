0a2fa782616bde3c4a47740a976dbfe8aaf7b63e
Mitchell Cohen <mitch@1password.com>
2019-12-02 22:04:34 -0500

    Updated fish config

--- .config/fish/aliases.fish
+++
@@ -1,5 +0,0 @@
-# dotfiles (see https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/)
-alias dotfiles="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
-
-# vscode
-type -q code-insiders; and alias code="code-insiders"
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,12 @@
-set -x PATH $HOME/anaconda3/bin $HOME/go/bin $PATH
+# PATH #
+source $HOME/.cargo/env
 
-# aliases
-set ALIASES_FILE "$HOME/.config/fish/aliases.fish"
-test -f $ALIASES_FILE; and source $ALIASES_FILE
+set GOPATH $HOME/go
+set PATH $HOME/go/bin $PATH
 
-# private config file (not synced to git)
-set PRIVATE_CONFIG_FILE "$HOME/.config/fish/config.private.fish"
-test -f $PRIVATE_CONFIG_FILE; and source $PRIVATE_CONFIG_FILE
+# ALIASES #
 
-# private aliases
-set PRIVATE_ALIASES_FILE "$HOME/.config/fish/aliases.private.fish"
-test -f $PRIVATE_ALIASES_FILE; and source $PRIVATE_ALIASES_FILE
+alias code code-insiders
 
-### Spacefish
-# Options: https://github.com/matchai/spacefish/blob/master/docs/Options.md
-# set SPACEFISH_PROMPT_ORDER time user dir host git package node docker ruby golang php rust haskell julia aws conda pyenv kubecontext exec_time line_sep battery jobs exit_code char
-set SPACEFISH_PROMPT_ORDER time user dir host git exec_time line_sep jobs exit_code char
+# PROMPT #
+source ("/Users/mitchell/.cargo/bin/starship" init fish --print-full-init | psub)
