b1d78216f3d4d7a8de37ca85a22be0d2e3d5e8be
Mitchell Cohen <mitch@1password.com>
2020-05-31 11:41:38 -0400

    added powerline-go

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
-# PATH #
+#### PATH ####
+
 source $HOME/.cargo/env
 
 set GOPATH $HOME/go
@@ -6,7 +7,24 @@ set PATH $HOME/go/bin $PATH
 
 # ALIASES #
 
-alias code code-insiders
+if type -q code-insiders
+    alias code code-insiders
+end
 
-# PROMPT #
-source ("$HOME/.cargo/bin/starship" init fish --print-full-init | psub)
+#### PROMPT ####
+
+# Starship
+# source ("$HOME/.cargo/bin/starship" init fish --print-full-init | psub)
+
+# Powerline-go
+function fish_prompt
+    set duration (math -s6 "$CMD_DURATION / 1000")
+    eval powerline-go \
+        -hostname-only-if-ssh \
+        -cwd-max-depth 2 \
+        -modules "host,ssh,cwd,perms,git,jobs,exit,root,duration" \
+        -duration $duration \
+        -duration-min 5 \
+        -condensed \
+        -shell bare
+end
